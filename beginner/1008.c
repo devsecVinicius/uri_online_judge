67a73735f0aa5d076dbc94bcb86e9c8f4c79f764
devsecVinicius <devsecvinicius@gmail.com>
2021-09-15 17:45:18 -0300

    Problem 1008

---
+++ beginner/1008.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main(){
+	
+	int number, hours;
+	float amount, salary;
+	
+	scanf("%i", &number);
+	scanf("%i", &hours);
+	scanf("%f", &amount);
+	
+	salary = hours * amount;
+	
+	printf("NUMBER = %i\n", number);
+	printf("SALARY =  U$ %.2f\n", salary);
+	
+	return 0;
+}
