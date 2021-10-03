a5fbc5120cf3bdf3808f1c2a7d9f0916d62e5ae5
Vinicius <devsecvinicius@gmail.com>
2021-10-03 11:30:11 -0300

    problem 1019 beginner

--- beginner/1019.c
+++ beginner/1019.c
@@ -1,1 +1,18 @@
+# include <stdio.h>
 
+int main(){
+	
+	int n = 0 ;
+	
+	scanf("%i", &n);
+	
+	int horas = (n / 60) / 60 ;
+	int minutos = (n / 60) % 60 ;
+	int segundos = (n % 60) % 60 ;
+	
+	printf("%i", horas);
+	printf(":%i:", minutos);
+	printf("%i\n", segundos);
+		
+	return 0;
+}
