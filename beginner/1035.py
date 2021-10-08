45e6af79443c57aa64972a2aa9ce6ac9d8393365
devsecVinicius <devsecvinicius@gmail.com>
2021-10-07 22:57:29 -0300

    problem 1035

--- beginner/1035.py
+++ beginner/1035.py
@@ -1,1 +1,12 @@
+numero = input().split(' ')
 
+a, b, c, d = numero
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+
+if(b>c and d > a and ((c+d) > (a+b)) and c>=0 and d>=0 and ((a%2) == 0) ):
+	print("Valores aceitos")
+else:
+	print("Valores nao aceitos")
