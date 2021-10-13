88c13fadb779893a94c32ce28aac8fd546634729
devsecVinicius <devsecvinicius@gmail.com>
2021-10-12 21:31:23 -0300

    problem 1036

--- beginner/1036.c
+++ beginner/1036.c
@@ -1,1 +1,25 @@
+# include <stdio.h>
+# include <math.h>
 
+int main(){
+	
+	double a,b,c;
+	
+	scanf("%lf", &a);
+	scanf("%lf", &b);
+	scanf("%lf", &c);
+	
+	double delta = pow(b,2) - (4*a*c);
+	
+	double r1 = ((b-(b+b)) + sqrt(delta)) / (2*a) ;
+	double r2 = ((b-(b+b)) - sqrt(delta)) / (2*a) ;
+	
+	if(delta <= 0 || a == 0 || c == 0)
+		printf("Impossivel calcular\n");
+	else{
+		printf("R1 = %.5lf\n", r1);
+		printf("R2 = %.5lf\n", r2);
+	}
+	
+	return 0;
+}
