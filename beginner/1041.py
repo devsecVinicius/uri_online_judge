3df886758dc290b647b7ee85e2c95f0b5555a0bf
devsecvinicius <devsecvinicius@gmail.com>
2021-12-24 17:01:37 -0300

    Problem 1041

--- beginner/1041.py
+++ beginner/1041.py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
 
+x, y = input().split(' ')
+x = float(x)
+y = float(y)
+
+if x == 0 and y != 0:
+	print('Eixo Y')
+if x != 0 and y == 0:
+	print('Eixo X')
+
+if x == 0 and y == 0:
+	print('Origem')
+else:
+	if x > 0 and y < 0:
+		print('Q4')
+	if x > 0 and y > 0:
+		print('Q1')	
+	if x < 0 and y < 0:
+		print('Q3')
+	if x < 0 and y > 0:
+		print('Q2')
