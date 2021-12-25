5523c9f319d40fadd32098a791a0c1f497240a7b
devsecvinicius <devsecvinicius@gmail.com>
2021-12-24 23:01:11 -0300

    Problem 1044 solved in C

--- beginner/1044.c
+++ beginner/1044.c
@@ -1,1 +1,27 @@
+# include <stdio.h>
 
+int main (){
+	int a, b, menor, maior;
+	float aux;
+	
+	scanf("%i", &a);
+	scanf("%i", &b);
+	
+	if(a > b){
+		menor = b;
+		maior = a;
+	}else{
+		menor = a;
+		maior = b;
+	}
+	
+	aux = maior / menor;
+	
+	if( maior == aux * menor){
+		printf("Sao Multiplos\n");
+	}else{
+		printf("Nao sao Multiplos\n");
+	}
+	
+	return 0;
+}
