5e132276c56e16cb76d7b23e2c1279690f7383b8
devsecvinicius <devsecvinicius@gmail.com>
2021-12-24 22:09:08 -0300

    Problem 1043 solved in python

--- beginner/1043.py
+++ beginner/1043.py
@@ -1,1 +1,7 @@
+a, b, c = input().split(' ')
+a, b, c = float(a), float(b), float(c)
 
+if(a < b + c and b < a + c and c < a + b):
+	print('Perimetro =', a+b+c)
+else:
+	print('Area =', ((a+b)*c) / 2)
