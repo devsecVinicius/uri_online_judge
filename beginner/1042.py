ed66a127c20c4b3f48b29216b371cc3326425275
devsecvinicius <devsecvinicius@gmail.com>
2021-12-24 21:43:23 -0300

    Problem 1042 solved in python

--- beginner/1042.py
+++ beginner/1042.py
@@ -1,1 +1,33 @@
+a, b, c = input().split(' ')
+a, b, c = int(a), int(b), int(c)
+#menor
+if a < b and a < c:
+	menor = a
+if b < a and b < c:
+	menor = b
+if c < a and c < b:
+	menor = c
+	
+# maior
+if a > b and a > c:
+	maior = a
+if b > a and b > c:
+	maior = b
+if c > a and c > b:
+	maior = c
 
+# medio
+if maior != a and menor != a:
+	medio = a
+if maior != b and menor != b:
+	medio = b
+if maior != c and menor != c:
+	medio = c
+	
+print(menor)
+print(medio)
+print(maior)
+print()
+print(a)
+print(b)
+print(c)
