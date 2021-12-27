938be7de3c6ef18f6b5372d83414529d01c1dd83
devsecvinicius <devsecvinicius@gmail.com>
2021-12-27 12:01:49 -0300

    Problem 1046 solved with Python

--- beginner/1046.py
+++ beginner/1046.py
@@ -1,1 +1,11 @@
+n1, n2 = input().split(" ")
+n1, n2 = int(n1), int(n2)
 
+duracao = 24 - n1
+duracao = duracao + n2
+
+if duracao > 24:
+	duracao = n2 - n1
+	print("O JOGO DUROU",duracao,"HORA(S)")
+else:
+	print("O JOGO DUROU",duracao,"HORA(S)")
