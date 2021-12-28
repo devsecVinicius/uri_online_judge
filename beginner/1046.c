5819bc42ecde7cfe563d5714b367010e0e20bc81
devsecvinicius <devsecvinicius@gmail.com>
2021-12-28 15:09:01 -0300

    Problem 1046 in C

--- beginner/1046.c
+++ beginner/1046.c
@@ -1,1 +1,19 @@
+# include <stdio.h>
 
+int main(){
+	int hora1, hora2, duracao;
+	
+	scanf("%i", &hora1);
+	scanf("%i", &hora2);
+
+	duracao = 24 - hora1;
+	duracao = duracao + hora2;
+
+	if(duracao > 24){
+		duracao = hora2 - hora1;
+	}
+
+	printf("O JOGO DUROU %i HORA(S)\n", duracao);
+
+	return 0;
+}
