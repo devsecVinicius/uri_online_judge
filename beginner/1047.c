8e8ad0b4445d394a6c44a7641364f1d1bec7eada
devsecvinicius <devsecvinicius@gmail.com>
2021-12-28 13:23:05 -0300

    Problem 1047 solved in C

--- beginner/1047.c
+++ beginner/1047.c
@@ -1,1 +1,31 @@
+# include <stdio.h>
 
+int main (){
+	int h1, h2, m1, m2, duracaoHoras, duracaoMinutos;
+	h1 = h2 = m1 = m2 = duracaoHoras = duracaoMinutos = 0;
+	
+	scanf("%i", &h1);
+	scanf("%i", &m1);
+	scanf("%i", &h2);
+	scanf("%i", &m2);
+	
+	
+	duracaoHoras = h2 - h1;
+	duracaoMinutos = m2 - m1;
+	
+	if(duracaoHoras == 0 && duracaoMinutos == 0){
+		duracaoHoras = 24;
+	}else{
+		if(duracaoMinutos < 0){
+			duracaoMinutos += 60;
+			duracaoHoras--;
+		}
+		if(duracaoHoras < 0){
+			duracaoHoras += 24;
+		}
+	}
+	
+	printf("O JOGO DUROU %i HORA(S) E %i MINUTO(S)\n", duracaoHoras, duracaoMinutos);
+	
+	return 0;
+}
