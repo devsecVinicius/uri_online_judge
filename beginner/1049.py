8f0be494d5c872109afa146636e857b45fcd4a69
devsecvinicius <devsecvinicius@gmail.com>
2021-12-28 21:18:06 -0300

    Problem 1049 solved in Python

--- beginner/1049.py
+++ beginner/1049.py
@@ -1,1 +1,26 @@
+primeiraPalavra = input()
+segundaPalavra = input()
+terceiraPalavra = input()
 
+if primeiraPalavra == "vertebrado":
+	if segundaPalavra == "ave":
+		if terceiraPalavra == "carnivoro":
+			print("aguia")
+		if terceiraPalavra == "onivoro":
+			print("pomba")
+	if segundaPalavra == "mamifero":
+		if terceiraPalavra == "onivoro":
+			print("homem")
+		if terceiraPalavra == "herbivoro":
+			print("vaca")
+if primeiraPalavra == "invertebrado":
+	if segundaPalavra == "inseto":
+		if terceiraPalavra == "hematofago":
+			print("pulga")
+		if terceiraPalavra == "herbivoro":
+			print("lagarta")
+	if segundaPalavra == "anelideo":
+		if terceiraPalavra == "hematofago":
+			print("sanguessuga")
+		if terceiraPalavra == "onivoro":
+			print("minhoca")
