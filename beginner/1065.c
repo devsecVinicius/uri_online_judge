ac60f7b996aaf8506074605cc2a1d135237d468b
devsecvinicius <devsecvinicius@gmail.com>
2021-12-30 19:02:53 -0300

    Problem 1065 solved in C

--- beginner/1065.c
+++ beginner/1065.c
@@ -1,1 +1,31 @@
+# include <stdio.h>
 
+int main(){
+	int n1, n2, n3, n4, n5, aux;
+	aux = n1 = n2 = n3 = n4 = n5 = 0;
+	
+	scanf("%i", &n1);
+	scanf("%i", &n2);
+	scanf("%i", &n3);
+	scanf("%i", &n4);
+	scanf("%i", &n5);
+	
+	if(n1 % 2 == 0)
+		aux++;
+	
+	if(n2 % 2 == 0)
+		aux++;
+	
+	if(n3 % 2 == 0)
+		aux++;
+	
+	if(n4 % 2 == 0)
+		aux++;
+	
+	if(n5 % 2 == 0)
+		aux++;
+	
+	printf("%i valores pares\n", aux);	
+	
+	return 0;
+}
