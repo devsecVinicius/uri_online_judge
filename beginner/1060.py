e0aa754a58b0d8bb36adf9add508c66c2d684cdd
devsecvinicius <devsecvinicius@gmail.com>
2021-12-30 09:46:34 -0300

    Problem 1060 solved in Python

--- beginner/1060.py
+++ beginner/1060.py
@@ -1,1 +1,27 @@
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+n4 = float(input())
+n5 = float(input())
+n6 = float(input())
+x = 0
 
+if n1 > 0:
+	x += 1
+
+if n2 > 0:
+	x += 1
+
+if n3 > 0:
+	x += 1
+	
+if n4 > 0:
+	x += 1	
+	
+if n5 > 0:
+	x += 1
+		
+if n6 > 0:
+	x += 1
+	
+print(f"{x} valores positivos")
