14e0ea50c084e284480a5f764f4095e4ec923b47
devsecvinicius <devsecvinicius@gmail.com>
2021-12-30 18:53:46 -0300

    Problem 1065 solved in Python

--- beginner/1065.py
+++ beginner/1065.py
@@ -1,1 +1,23 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+n5 = int(input())
+aux = 0
 
+if n1 % 2 == 0:
+	aux += 1
+
+if n2 % 2 == 0:
+	aux += 1
+
+if n3 % 2 == 0:
+	aux += 1
+	
+if n4 % 2 == 0:
+	aux += 1	
+	
+if n5 % 2 == 0:
+	aux += 1
+	
+print(aux,"valores pares")
