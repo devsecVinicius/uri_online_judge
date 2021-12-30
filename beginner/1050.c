3754205df15af431a8f87a076e6ad8cc73a6eaf8
devsecvinicius <devsecvinicius@gmail.com>
2021-12-29 22:33:35 -0300

    Problem 1050

--- beginner/1050.c
+++ beginner/1050.c
@@ -1,1 +1,52 @@
+# include <stdio.h>
 
+int main(){
+	int ddd;
+	
+	scanf("%i", &ddd);
+	
+	if(ddd == 61){
+		printf("Brasilia\n");
+		goto end;
+	}
+	
+	if(ddd == 71){
+		printf("Salvador\n");
+		goto end;
+	}
+	
+	if(ddd == 11){
+		printf("Sao Paulo\n");
+		goto end;
+	}
+	
+	if(ddd == 21){
+		printf("Rio de Janeiro\n");
+		goto end;
+	}
+	
+	if(ddd == 32){
+		printf("Juiz de Fora\n");
+		goto end;
+	}
+	
+	if(ddd == 19){
+		printf("Campinas\n");
+		goto end;
+	}
+	
+	if(ddd == 27){
+		printf("Vitoria\n");
+		goto end;
+	}
+		
+	if(ddd == 31){
+		printf("Belo Horizonte\n");
+		goto end;
+	}
+	
+	printf("DDD nao cadastrado\n");
+	
+	end:
+	return 0;
+}
