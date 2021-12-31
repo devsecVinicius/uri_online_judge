856c7652e909ab10ec2ff40c368abc17b63078e1
devsecvinicius <devsecvinicius@gmail.com>
2021-12-31 09:30:34 -0300

    Problem 1067

--- beginner/1067.c
+++ beginner/1067.c
@@ -1,1 +1,17 @@
+# include <stdio.h>
 
+int main(){
+	int numero;
+	scanf("%i", &numero);
+	
+	int i = 0;
+	
+	while(i <= numero){
+		if( i % 2 == 1){
+			printf("%i\n", i);
+		}
+		i += 1;
+	}
+	
+	return 0;
+}
--- beginner/1067.py
+++ beginner/1067.py
@@ -1,1 +1,8 @@
+numero = int(input())
 
+i = 0
+
+while i <= numero:
+	if i % 2 == 1:
+		print(i)
+	i += 1
