4b68f66d240e75fd2d7db2f1e9346161b85bad9d
devsecvinicius <devsecvinicius@gmail.com>
2021-12-31 11:34:01 -0300

    Problem 1071 in Python

--- beginner/1071.py
+++ beginner/1071.py
@@ -1,1 +1,20 @@
+x = int(input())
+y = int(input())
+soma = 0
 
+if x < y:
+	x += 1
+	while x < y:
+		if x % 2 == 1:
+			soma += x 
+		x += 1
+		
+if x > y:
+	y += 1
+	while y < x:
+		if y % 2 == 1:
+			soma += y 
+		y += 1
+
+		
+print(soma)
