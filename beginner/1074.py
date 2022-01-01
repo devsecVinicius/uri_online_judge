cf2a7483c42e14640cf8939acf903dedad9dda24
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 12:15:34 -0300

    Problem 1074

--- beginner/1074.py
+++ beginner/1074.py
@@ -1,1 +1,21 @@
+n = int(input())
+i = 0
+numbers = []
 
+while i < n:
+	numbers.append(int(input()))
+	i += 1
+
+for x in numbers:
+	if x == 0:
+		print("NULL")
+	if x % 2 == 0:
+		if x > 0:
+			print("EVEN POSITIVE")  
+		if x < 0:
+			print("EVEN NEGATIVE")  
+	if x % 2 == 1:
+		if x > 0:
+			print("ODD POSITIVE")  
+		if x < 0:
+			print("ODD NEGATIVE")  
