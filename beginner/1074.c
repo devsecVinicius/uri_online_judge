29f7318165afbbbbddc9bebc0ef58728609bad47
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 13:09:10 -0300

    Problem 1074 in C

--- beginner/1074.c
+++ beginner/1074.c
@@ -1,1 +1,39 @@
+# include <stdio.h>
 
+int main(){
+	
+	int n, i, numbers[10000] = {};
+	
+	scanf("%i", &n);
+	
+	for( i = 0 ; i < n ; ++i ){
+		scanf("%i", &numbers[i]);
+	}
+	
+	for( i = 0 ; i < n ; ++i ){
+		if(numbers[i] == 0)
+			printf("NULL\n");
+		
+		if(numbers[i] % 2 == 0){
+			if( numbers[i] > 0 ){
+				printf("EVEN POSITIVE\n");
+			}
+				
+			if( numbers[i] < 0 ){
+				printf("EVEN NEGATIVE\n");
+			}
+		}
+		
+		if(numbers[i] % 2 == 1 || numbers[i] % 2 == -1){
+			if( numbers[i] > 0 ){
+				printf("ODD POSITIVE\n");
+			}
+			
+			if( numbers[i] < 0 ){
+				printf("ODD NEGATIVE\n");
+			}
+		}
+	}
+	
+	return 0;
+}
