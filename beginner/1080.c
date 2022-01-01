39069ed06fd37cb33cd3daa15833ff73d3e4edb8
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 15:27:50 -0300

    1080 - C

--- beginner/1080.c
+++ beginner/1080.c
@@ -1,1 +1,20 @@
+# include <stdio.h>
 
+int main(){
+	int i, highest, inputPosition, n;
+	i = highest = inputPosition = n = 0;
+	
+	for( i = 1 ; i <= 100 ; ++i){
+		scanf("%i", &n);
+		if( n > highest){
+			highest = n;
+			inputPosition = i;
+		}
+	}
+	
+	printf("%i\n", highest);
+	printf("%i\n", inputPosition);
+	
+	
+	return 0;
+}
