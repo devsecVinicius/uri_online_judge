1d359562f979bb5701eb61e72b131140232c7d56
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 11:54:08 -0300

    Problema 1073 resolvido em C

--- beginner/1073.c
+++ beginner/1073.c
@@ -1,1 +1,15 @@
+# include <stdio.h>
+# include <math.h>
 
+int main(){
+	int n, i;
+	
+	scanf("%i", &n);
+	
+	for( i = 0 ; i <= n ; ++i, ++i){
+		if( i != 0)
+			printf("%i^2 = %.0lf\n", i, pow(i, 2));
+	}
+
+	return 0;
+}
