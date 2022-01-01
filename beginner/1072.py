3b7578b62b03534673ea4d9877d93867757c636c
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 11:00:57 -0300

    Problem 1072 solved in Python

--- beginner/1072.py
+++ beginner/1072.py
@@ -1,1 +1,18 @@
+n = int(input())
+i = 0
+numbers = []
+iN = 0
+out = 0
 
+while i < n:
+	numbers.append(int(input()))
+	i += 1
+	
+for x in numbers:
+	if x >= 10 and x <= 20:
+		iN += 1
+	else:
+		out += 1
+		
+print(f"{iN} in")
+print(f"{out} out")
