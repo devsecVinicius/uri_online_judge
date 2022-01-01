c8974cd324562d0e9e5b698fad50c09163538009
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 14:35:48 -0300

    1079 - Python

--- beginner/1079.py
+++ beginner/1079.py
@@ -1,1 +1,17 @@
+n = int(input())
+numbers = []
+i = 0
 
+while i < n :
+	aux1, aux2, aux3 = input().split(" ")
+	aux1, aux2, aux3 = float(aux1), float(aux2), float(aux3)
+	numbers.append(aux1)
+	numbers.append(aux2)
+	numbers.append(aux3)
+	i += 1
+else:
+	i = 0
+	
+while i < n*3:
+	print(f"{( ( (numbers[i] * 2) + (numbers[i+1] * 3) + (numbers[i+2] * 5 ) ) / 10 ):.1f}" )
+	i += 3
