528df6e7db81c75bda5d39b44a06899c2e1dd1a7
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 10:12:08 -0300

    Problem 1071 solved in C

--- beginner/1071.c
+++ beginner/1071.c
@@ -1,1 +1,32 @@
+# include <stdio.h>
 
+int main(){
+	int x, y, soma;
+	scanf("%i", &x);
+	scanf("%i", &y);
+	soma = 0;
+	
+	if(x < y){
+		x += 1;
+		while(x < y){
+			if(x % 2 == 1 || x % 2 == -1)
+				soma += x; 
+			x += 1;
+		}
+	}
+	
+	if(x > y){
+		y += 1;
+		while(y < x){
+			if(y % 2 == 1 || y % 2 == -1){
+				soma += y; 
+			}
+		y += 1;
+		}
+	}
+
+	
+	printf("%i\n", soma);
+	
+	return 0;
+}
