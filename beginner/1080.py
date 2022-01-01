e40f12f2da4513ab2579189594983ae31bfda864
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 14:49:56 -0300

    1080 - Python

--- beginner/1080.py
+++ beginner/1080.py
@@ -1,1 +1,14 @@
+n = 0
+highest = 0
+inputPosition = 0
+i = 0
 
+while i < 10:
+	n = int(input())
+	if n > highest:
+		highest = n
+		inputPosition = i
+	i += 1
+	
+print(highest)
+print(inputPosition)
