59f0b57c33bdd22081f1739b30336e6b2bb444dd
devsecvinicius <devsecvinicius@gmail.com>
2022-01-01 11:32:29 -0300

    Problem 1072

--- beginner/1072.c
+++ beginner/1072.c
@@ -1,1 +1,22 @@
+# include <stdio.h>
 
+int main (){
+	int i, n, numbers[10000] = {}, in = 0, out = 0;
+	
+	scanf("%i", &n);
+	
+	for( i = 0 ; i < n ; ++i)
+		scanf("%i", &numbers[i]);
+	
+	for( i = 0 ; i < n ; ++i ){
+		if(numbers[i] >= 10 && numbers[i] <= 20)
+			++in;
+		else
+			++out;
+	}
+	
+	printf("%i in\n", in);
+	printf("%i out\n", out);
+	
+	return 0;
+}
