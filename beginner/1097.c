2977875e9aa3cb89efc9fb279c1c0e19674939e3
devsecvinicius <devsecvinicius@gmail.com>
2022-01-02 16:24:42 -0300

    1097 - C

--- beginner/1097.c
+++ beginner/1097.c
@@ -1,1 +1,15 @@
+# include <stdio.h>
 
+int main(){
+	int i = 1, j =7;
+	
+	while(i <=9){
+		printf("I=%i J=%i\n", i, j);
+		printf("I=%i J=%i\n", i, j - 1);
+		printf("I=%i J=%i\n", i, j - 2);
+		i += 2;
+		j = i + 6;
+	}
+	
+	return 0;
+}
