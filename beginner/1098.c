b061d192efcacea4ab3e3cf354c33dd32e4cd756
devsecvinicius <devsecvinicius@gmail.com>
2022-01-03 09:19:56 -0300

    1098 - C

--- beginner/1098.c
+++ beginner/1098.c
@@ -1,1 +1,24 @@
+# include <stdio.h>
 
+int main(){
+	float i = 0, j = 1;
+	
+	while( i < 2 ){
+		if( i == 0 || i == 1 ){
+			printf("I=%.0f J=%.0f\n", i, j );
+			printf("I=%.0f J=%.0f\n", i, j + 1);
+			printf("I=%.0f J=%.0f\n", i, j + 2);
+		}else{
+			printf("I=%.1f J=%.1f\n", i, j );
+			printf("I=%.1f J=%.1f\n", i, j + 1);
+			printf("I=%.1f J=%.1f\n", i, j + 2);
+		}
+		i += 0.2;
+		j += 0.2;
+	}
+	printf("I=%.0f J=%.0f\n", i, j );
+	printf("I=%.0f J=%.0f\n", i, j + 1);
+	printf("I=%.0f J=%.0f\n", i, j + 2);
+	
+	return 0;
+}
