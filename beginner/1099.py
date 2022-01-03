5e5e33456bcba4ea7488d587c2fccb97db319487
devsecvinicius <devsecvinicius@gmail.com>
2022-01-03 10:59:09 -0300

    1099 - Python

--- beginner/1099.py
+++ beginner/1099.py
@@ -1,1 +1,28 @@
+n = int(input())
+i = 0
+output = []
 
+while i < n:
+	soma = 0
+	aux1, aux2 = input().split(" ")
+	aux1, aux2 = int(aux1), int(aux2)
+	if aux1 == aux2:
+		output.append(0)
+	if aux1 < aux2:
+		aux1 += 1
+		while aux1 < aux2:
+			if aux1 % 2 == 1:
+				soma += aux1
+			aux1 += 1
+		output.append(soma)
+	if aux2 < aux1:
+		aux2 += 1
+		while aux2 < aux1:
+			if aux2 % 2 == 1:
+				soma += aux2
+			aux2 += 1
+		output.append(soma)
+	i += 1
+		 
+for x in output:
+	print(x)
