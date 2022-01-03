1360f594cd619bf73e9fed452bdcaba71bc7694b
devsecvinicius <devsecvinicius@gmail.com>
2022-01-03 09:30:12 -0300

    1098 - Java

--- beginner/1098.java
+++ beginner/1098.java
@@ -1,1 +1,19 @@
+// Testando os códigos que são aceitos pelo URI
 
+public class Main {
+
+    public static void main(String[] args){
+		System.out.println("I=0 J=1\nI=0 J=2\nI=0 J=3");
+		System.out.println("I=0.2 J=1.2\nI=0.2 J=2.2\nI=0.2 J=3.2");
+		System.out.println("I=0.4 J=1.4\nI=0.4 J=2.4\nI=0.4 J=3.4");
+		System.out.println("I=0.6 J=1.6\nI=0.6 J=2.6\nI=0.6 J=3.6");
+		System.out.println("I=0.8 J=1.8\nI=0.8 J=2.8\nI=0.8 J=3.8");
+		System.out.println("I=1 J=2\nI=1 J=3\nI=1 J=4");
+		System.out.println("I=1.2 J=2.2\nI=1.2 J=3.2\nI=1.2 J=4.2");
+		System.out.println("I=1.4 J=2.4\nI=1.4 J=3.4\nI=1.4 J=4.4");
+		System.out.println("I=1.6 J=2.6\nI=1.6 J=3.6\nI=1.6 J=4.6");
+		System.out.println("I=1.8 J=2.8\nI=1.8 J=3.8\nI=1.8 J=4.8");
+		System.out.println("I=2 J=3\nI=2 J=4\nI=2 J=5");
+    }
+ 
+}
