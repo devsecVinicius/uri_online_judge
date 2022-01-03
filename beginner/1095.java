fb47de42776e76a2395d3e6b71b9c89e8ecbcd08
devsecvinicius <devsecvinicius@gmail.com>
2022-01-03 09:40:30 -0300

    1095 - Java

--- beginner/1095.java
+++ beginner/1095.java
@@ -1,1 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+		int i = 1, j = 60;
+		
+		for( ; j >= 0 ; i+= 3, j -= 5){
+			System.out.println("I="+i+" J="+j);
+		}
+    }
+ 
+}
 
