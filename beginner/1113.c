3e3b685540c650a597546f7850d3482ff924525d
devsecvinicius <devsecvinicius@gmail.com>
2022-01-06 10:24:06 -0300

    1113 - C

--- beginner/1113.c
+++ beginner/1113.c
@@ -1,1 +1,21 @@
+# include <stdio.h>
 
+int main(){
+	int n1 = 0, n2 = 0;
+	
+	while(1){
+		scanf("%i", &n1);
+		scanf("%i", &n2);
+		
+		if( n1 == n2 )
+			break;
+		
+		if( n1 > n2 )
+			printf("Decrescente\n");
+		
+		if( n2 > n1 )
+			printf("Crescente\n");
+	}
+	
+	return 0;
+}
