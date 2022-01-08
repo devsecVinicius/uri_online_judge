f4391b466089a0410599e5f1dd3d3bd016c947e9
devsecvinicius <devsecvinicius@gmail.com>
2022-01-07 21:40:51 -0300

    1115 - C

--- beginner/1115.c
+++ beginner/1115.c
@@ -1,1 +1,24 @@
+# include <stdio.h>
 
+int main(){
+	
+	int x = 0, y = 0;
+	
+	while(1){
+		scanf("%i", &x);
+		scanf("%i", &y);
+		
+		if( x > 0 && y > 0 )
+			printf("primeiro\n");
+		if( x < 0 && y > 0 )
+			printf("segundo\n");
+		if( x < 0 && y < 0 )
+			printf("terceiro\n");
+		if( x > 0 && y < 0 )
+			printf("quarto\n");
+		if( x == 0 || y == 0 )
+			break;
+	}
+	
+	return 0;
+}
