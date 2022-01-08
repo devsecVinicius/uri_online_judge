e1dfb5067d2402b94da2fdb4bace817a60a3b59b
devsecvinicius <devsecvinicius@gmail.com>
2022-01-07 21:17:32 -0300

    1114 - C

--- beginner/1114.c
+++ beginner/1114.c
@@ -1,1 +1,17 @@
+# include <stdio.h>
 
+int main(){
+	
+	int password;
+	while(1){
+		scanf("%i", &password);
+		 
+		if( password == 2002 ){
+			printf("Acesso Permitido\n");
+			break;
+		}else
+			printf("Senha Invalida\n");
+	}
+	
+	return 0;
+}
