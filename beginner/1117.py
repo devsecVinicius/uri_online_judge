fc0536272c3585701c0ee1be77bc77ba35f16890
devsecvinicius <devsecvinicius@gmail.com>
2022-01-11 21:44:54 -0300

    1117 - Python

--- beginner/1117.py
+++ beginner/1117.py
@@ -1,1 +1,11 @@
+score = []
 
+while True:
+	aux = float(input())
+	if aux < 0 or aux > 10:
+		print("nota invalida")
+	else:
+		score.append(aux)
+	if len(score) > 1:
+		break
+print(f"media = {((score[0]+score[1])/2):.2f}")
