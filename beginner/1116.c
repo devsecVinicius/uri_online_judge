7996d4c103a8f2be06a688a7b13025fd6e121c03
devsecvinicius <devsecvinicius@gmail.com>
2022-01-11 21:20:30 -0300

    1116 - C

--- beginner/1116.c
+++ beginner/1116.c
@@ -1,1 +1,22 @@
+# include <stdio.h>
 
+int main(){
+	
+	int n = 0;
+	
+	scanf("%i", &n);
+	int i = 0;
+	
+	float x,y;
+	for(i =0 ; i < n ; ++i ){
+		scanf("%f", &x);
+		scanf("%f", &y);
+		
+		if(y == 0)
+			printf("divisao impossivel\n");
+		else
+			printf("%.1f\n", (x/y) );
+	}
+	
+	return 0;
+}
