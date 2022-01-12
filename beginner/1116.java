0bdea0758b368995136bc4e218aad699357649f6
devsecvinicius <devsecvinicius@gmail.com>
2022-01-11 21:30:26 -0300

    1116 - Java

--- beginner/1116.java
+++ beginner/1116.java
@@ -1,1 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+		int n;
+		Scanner scanner = new Scanner(System.in);
+		n = scanner.nextInt();
+		
+		double x, y;
+		
+		for(int i = 0 ; i < n ; i ++){
+			x = scanner.nextDouble();
+			y = scanner.nextDouble();
+		
+			if( y == 0){
+				System.out.println("divisao impossivel");
+				continue;
+			}
+			
+			System.out.println(x/y);
+		}
+ 
+    }
+ 
+}
 
