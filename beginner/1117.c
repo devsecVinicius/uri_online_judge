5549d7765ebb833359a00c7ed1123a17f5c94480
devsecvinicius <devsecvinicius@gmail.com>
2022-01-15 21:36:40 -0300

    1117 - C

--- beginner/1117.c
+++ beginner/1117.c
@@ -1,1 +1,21 @@
+# include <stdio.h>
 
+int main(){
+
+	float score1, score2, aux;
+	int i;
+	score1 = score2 = aux = 0;
+	
+	for( i = 0 ; i < 2 ; ){
+		scanf("%f", &aux);
+		if( aux > 10 || aux < 0 ){
+			printf("nota invalida\n");
+		}else{
+			score1 = score2;
+			score2 = aux;
+			++i;
+		}
+	}
+	printf("media = %.2f\n", (score1+score2) / 2 );
+	return 0;
+}
