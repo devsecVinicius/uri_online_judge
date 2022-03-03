67c7959abef6bad81d780c4f9f7f646e125bce31
Vinicius Ribeiro de Lima <vinicius.r17@aluno.ifsc.edu.br>
2022-03-02 22:12:51 -0300

    1117 - Java

--- beginner/1117.java
+++ beginner/1117.java
@@ -1,1 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+		
+		Scanner scanner = new Scanner(System.in);
+		float score1, score2, aux;
+		int i;
+		score1 = score2 = aux = 0;
+		for( i = 0 ; i < 2 ; ){
+			aux = scanner.nextFloat();
+			if( aux > 10 || aux < 0 ){
+				System.out.println("nota invalida");
+			}
+			else{
+				score1 = score2;
+				score2 = aux;
+				++i;
+			}
+		}
+		System.out.println("media = " + (score1+score2) / 2 );
+    }
+ 
+}
