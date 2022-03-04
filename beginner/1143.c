03300b774320d59975c974004351133fc7be8e66
devsecVinicius <>
2022-03-04 18:11:19 -0300

    1143 - C

--- beginner/1143.c
+++ beginner/1143.c
@@ -1,1 +1,18 @@
+# include <stdio.h>
+# include <math.h>
 
+int main(){
+    double n, i;
+    n = i = 0;
+
+    scanf("%lf", &n);
+
+    i = i + 1;
+
+    while( i <= n ){
+        printf("%.0f %.0f %.0f\n", i, pow(i,2), pow(i,3) );
+        i = i + 1;
+    }
+
+    return 0;
+}
