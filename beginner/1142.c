4dcd0d572af21f09503aafeacbbf30c6638724b9
devsecVinicius <>
2022-03-04 15:16:42 -0300

    1142 - C

--- beginner/1142.c
+++ beginner/1142.c
@@ -1,1 +1,17 @@
+# include <stdio.h>
 
+int main(){
+    int n, aux, i;
+    n = aux = i = 0;
+    aux++;
+
+    scanf("%i", &n);
+
+    while( i < n ){
+        printf("%i %i %i PUM\n", aux, aux+1, aux+2);
+        aux = aux + 4;
+        ++i;
+    }
+
+    return 0;
+}
