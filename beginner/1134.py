8d29a1ce38ce72213d0976c49ebab153568d4368
Vinicius Ribeiro de Lima <vinicius.r17@aluno.ifsc.edu.br>
2022-03-04 14:20:25 -0300

    1134 - Python

--- beginner/1134.py
+++ beginner/1134.py
@@ -1,1 +1,20 @@
+alcool = 0
+gasolina = 0
+diesel = 0
 
+while True:
+    aux = int(input())
+
+    if aux == 1:
+        alcool += 1
+    if aux == 2:
+        gasolina += 1
+    if aux == 3:
+        diesel += 1
+    if aux == 4:
+        break
+
+print("MUITO OBRIGADO")
+print("Alcool:", alcool)
+print("Gasolina:", gasolina)
+print("Diesel:", diesel)
