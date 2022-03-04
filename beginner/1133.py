411ca6e8e0eb7a22bf7bca5021b69f2df5e2a81b
Vinicius Ribeiro de Lima <vinicius.r17@aluno.ifsc.edu.br>
2022-03-04 13:44:37 -0300

    1133 - Python

--- beginner/1133.py
+++ beginner/1133.py
@@ -1,1 +1,12 @@
+x = int(input())
+y = int(input())
 
+while x < y -1:
+    x += 1
+    if x % 5 == 2 or x % 5 == 3:
+        print(x)
+
+while y < x -1:
+    y += 1
+    if y % 5 == 2 or y % 5 == 3:
+        print(y)
