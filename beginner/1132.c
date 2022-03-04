738eab11507439dd2c405fbb113560c97a92faeb
Vinicius Ribeiro de Lima <vinicius.r17@aluno.ifsc.edu.br>
2022-03-04 01:08:52 -0300

    1132 - C

--- beginner/1132.c
+++ beginner/1132.c
@@ -1,1 +1,32 @@
+# include <stdio.h>
 
+int main(){
+
+    int x, y, sum;
+    x = y = sum = 0 ;
+
+    scanf("%i", &x);
+    scanf("%i", &y);
+
+    if( x < y ){
+        while( x <= y ){
+            if( ( x % 13 ) != 0 ){
+                sum = sum + x;
+            }
+            x = x + 1;
+        }
+    }
+    else
+        if( y < x ){
+            while( y <= x ){
+                if( ( y % 13 ) != 0 ){
+                    sum = sum + y;
+                }
+                y = y + 1;
+            }
+        }
+
+    printf("%i\n", sum);
+
+    return 0;
+}
