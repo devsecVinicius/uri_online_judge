400dfc4d512816a89e99b9a1b694f71d49ee72de
Vinicius Ribeiro de Lima <vinicius.r17@aluno.ifsc.edu.br>
2022-03-04 14:05:46 -0300

    1133 - C

--- beginner/1133.c
+++ beginner/1133.c
@@ -1,1 +1,24 @@
+# include <stdio.h> 
 
+int main(){
+    int x, y;
+
+    scanf("%i", &x);
+    scanf("%i", &y);
+
+    while( x < y - 1 ){
+        x++;
+        if( x % 5 == 2 || x % 5 == 3){
+            printf("%i\n", x);
+        }
+    }
+
+    while( y < x - 1 ){
+        y++;
+        if( y % 5 == 2 || y % 5 == 3){
+            printf("%i\n", y);
+        }
+    }
+
+    return 0;
+}
