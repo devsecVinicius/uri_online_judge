999ddd048a53ef26a3fb5382bc4b271de68f8c71
Vinicius Ribeiro de Lima <vinicius.r17@aluno.ifsc.edu.br>
2022-03-04 00:26:48 -0300

    1132 - Python

--- beginner/1132.py
+++ beginner/1132.py
@@ -1,1 +1,16 @@
+x = int(input())
+y = int(input())
+sum = 0
 
+if x < y:
+    while x <= y:
+        if (x % 13) != 0:
+            sum += x
+        x += 1
+elif x > y:
+    while y <= x:
+        if (y % 13) != 0:
+            sum += y
+        y += 1
+
+print(sum)
