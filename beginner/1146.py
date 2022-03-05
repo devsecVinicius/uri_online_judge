bf7f5b748a49f366454008bf390cd16ecfa62ec4
devsecVinicius <>
2022-03-05 17:58:34 -0300

    1146 - Python

--- beginner/1146.py
+++ beginner/1146.py
@@ -1,1 +1,14 @@
+n = 1
 
+while n != 0:
+    n = int(input())
+    i = 1
+    
+    while i <= n:
+        if i == n:
+            print(i, end='')
+        else:
+            print(i,'',end='')
+        i += 1
+    if not(n == 0):
+        print()
