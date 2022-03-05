1d47d52dc05722c319ada49927071f95aa1874e9
devsecVinicius <>
2022-03-05 16:42:05 -0300

    1145 - Python

--- beginner/1145.py
+++ beginner/1145.py
@@ -1,1 +1,16 @@
+x, y = input().split(' ')
+x, y = int(x), int(y)
+i, j = 1, 0
 
+while i < y:
+    j = 0
+
+    while j < x:
+        if not(j == x-1):
+            print(i,'',end='')
+        else:
+            print(i,end='')
+        j += 1
+        i += 1
+    
+    print()
