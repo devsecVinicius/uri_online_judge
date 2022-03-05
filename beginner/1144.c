36e08adde4806a9f52d6ed8dc628438e55993bcc
devsecVinicius <>
2022-03-05 15:39:24 -0300

    1144 - C

--- beginner/1144.c
+++ beginner/1144.c
@@ -1,1 +1,17 @@
+# include <stdio.h>
+# include <math.h>
 
+int main(){
+
+    double n, i = 1;
+
+    scanf("%lf", &n);
+
+    for( ; i <= n ; ++i ){
+        printf("%.0f %.0f %.0f\n", i, pow(i,2), pow(i,3) );
+        printf("%.0f %.0f %.0f\n", i, (pow(i,2)+1), (pow(i,3)+1) );
+    }
+
+
+    return 0;
+}
