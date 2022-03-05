36e2c5dca68ff9de5955ecfd06c98d706cff1cd6
devsecVinicius <>
2022-03-05 17:38:07 -0300

    1145 - C

--- beginner/1145.c
+++ beginner/1145.c
@@ -1,1 +1,29 @@
+# include <stdio.h>
 
+int main(){
+
+    int x, y;
+
+    scanf("%i", &x);
+    scanf("%i", &y);
+
+    int i = 1, j = 0;
+
+    while( i < y ){
+        j = 0;
+
+        while( j < x ){
+            if( !(j == x-1) ){
+                printf("%i ", i);
+            }else{
+                printf("%i", i);
+            }
+            ++j;
+            ++i;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
