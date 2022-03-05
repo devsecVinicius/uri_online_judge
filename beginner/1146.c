20f058a955ef0e798f6298f59474179216ac3448
devsecVinicius <>
2022-03-05 18:09:43 -0300

    1146 - C

--- beginner/1146.c
+++ beginner/1146.c
@@ -1,1 +1,27 @@
+# include <stdio.h>
 
+int main(){
+
+    int n, i = 1;
+
+    while( n != 0 ){
+        
+        scanf("%i", &n);
+        i = 1;
+        
+        while( i <= n ){
+            if( i == n ){
+                printf("%i", i);
+            }else{
+                printf("%i ", i);
+            }
+            ++i;
+        }
+
+        if( !( n == 0 ) ){
+            printf("\n");
+        }
+
+    }
+    return 0;
+}
