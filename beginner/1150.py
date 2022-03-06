770bcf033ac22be722538085fdf835febd42d40f
devsecVinicius <>
2022-03-06 19:54:51 -0300

    1150 - Python

--- beginner/1150.py
+++ beginner/1150.py
@@ -1,1 +1,18 @@
+x = int(input())
 
+z = x-1
+
+while z <= x:
+    z = int(input())
+
+i = 1
+
+mylist = [x,]
+aux = x
+
+while (sum(mylist)) < z:
+    aux += 1
+    mylist.append(aux)
+    i += 1
+
+print(i)
