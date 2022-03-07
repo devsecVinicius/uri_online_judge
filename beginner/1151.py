0791ea621dc78b87df4c7b455b2afc97df1d1a34
devsecVinicius <>
2022-03-07 12:03:45 -0300

    1151 - Python

--- beginner/1151.py
+++ beginner/1151.py
@@ -1,1 +1,12 @@
+n = int(input())
 
+listaFibonacci = [0, 1, 1]
+
+while (len(listaFibonacci)) < n:
+    listaFibonacci.append(listaFibonacci[-1]+listaFibonacci[-2])
+
+for fibonacciNumber in listaFibonacci:
+    if fibonacciNumber == listaFibonacci[-1]:
+        print(fibonacciNumber)
+    else:
+        print(fibonacciNumber,"",end='')
