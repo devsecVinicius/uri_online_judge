19c2b5ef018cb99a7e61f977b21affe6c99ee278
devsecVinicius <77497869+devsecVinicius@users.noreply.github.com>
2022-03-13 23:07:13 -0300

    Update 1153.kt

--- beginner/1153.kt
+++ beginner/1153.kt
@@ -1,1 +1,15 @@
+import java.util.*
 
+fun main() {
+	var fat: Int = 1
+    var n: Int = 6
+    
+    n = readln().toInt()
+
+	while( n > 1 ){
+  		fat = fat * n
+        n = n - 1
+    }
+    
+    println(fat)
+}
