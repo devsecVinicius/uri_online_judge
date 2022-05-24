50d6e0686aa46db23c5b644250de79e979aa6c9d
vinicius <viniciusribeirodelima13@gmail.com>
2022-05-24 20:51:21 -0300

    1153 - C

--- beginner/1153.c
+++ beginner/1153.c
@@ -1,1 +1,19 @@
+# include <stdio.h>
 
+int main(){
+
+    int n, fatorial;
+
+    scanf("%i", &n);
+    fatorial = n;
+    n--;
+
+    for ( ; n > 0 ; n-- )
+    {
+        fatorial = fatorial * n;
+    }
+    
+    printf("%i\n", fatorial);
+
+    return 0;
+}
