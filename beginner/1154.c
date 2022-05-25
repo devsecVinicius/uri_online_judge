3bcb418eabfb0f6b7172182fed4a012072e43163
vinicius <viniciusribeirodelima13@gmail.com>
2022-05-25 15:04:59 -0300

    1154 - C

--- beginner/1154.c
+++ beginner/1154.c
@@ -1,1 +1,25 @@
+# include <stdio.h>
 
+int main(){
+
+    double n, soma = 0, quantidade_de_inputs = 0;
+    double resultado;
+
+    while(1)
+    {
+        scanf("%lf", &n);
+        if( n <= 0)
+        {
+            break;
+        }
+        
+        soma += n;
+        quantidade_de_inputs++;
+
+    }
+    resultado = soma/quantidade_de_inputs;
+
+    printf("%.2f\n", resultado);
+
+    return 0;
+}
