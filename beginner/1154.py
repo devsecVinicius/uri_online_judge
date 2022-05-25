16979f67178435aee487a5fb156f0d65cfb18083
vinicius <viniciusribeirodelima13@gmail.com>
2022-05-24 21:42:38 -0300

    1154 - Python

--- beginner/1154.py
+++ beginner/1154.py
@@ -1,1 +1,12 @@
+quantidade_de_inputs = 0
+soma = 0
 
+while True:
+    n = int(input())
+    if n <= 0:
+        break
+
+    soma += n
+    quantidade_de_inputs += 1
+
+print(f'{soma/quantidade_de_inputs:.2f}')
