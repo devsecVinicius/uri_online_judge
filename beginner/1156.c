5e4908a9f4befa6143792b47f856227eef056a7c
vinicius <viniciusribeirodelima13@gmail.com>
2022-05-27 14:48:25 -0300

    1156 - C

--- beginner/1156.c
+++ beginner/1156.c
@@ -1,1 +1,21 @@
+# include <stdio.h>
 
+int main(){
+
+    double s, n1, n2;
+    
+    s = 1;
+    n1 = 3;
+    n2 = 2;
+
+    while ( n1 < 39 )
+    {
+        s += n1 / n2;
+        n1 += 2;
+        n2 = n2 * 2;
+    }
+    
+    printf("%.2f\n", s);
+
+    return 0;
+}
