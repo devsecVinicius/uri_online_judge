a6a6bd690586db5ef7254ed2a25f1fdb2ad482a0
vinicius <viniciusribeirodelima13@gmail.com>
2022-05-27 18:12:47 -0300

    1157 - Python

--- beginner/1157.py
+++ beginner/1157.py
@@ -1,1 +1,15 @@
+n = int(input())
+contador = n
+lista_de_divisores = []
 
+while contador > 0:
+    resultado_da_divisao = n % contador
+    if resultado_da_divisao == 0:
+        lista_de_divisores.append(contador)
+    
+    contador -= 1
+
+lista_de_divisores.reverse()
+
+for l in lista_de_divisores:
+    print(l)
